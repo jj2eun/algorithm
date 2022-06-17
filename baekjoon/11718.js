090d6517eb55b1a2c2975aac51c4d0bd3aad1e14
jieun <rlawldms6076@gmail.com>
2022-06-17 21:41:36 +0900

    Add 11718

---
+++ baekjoon/11718.js
@@ -0,0 +1,8 @@
+/**
+ * 그대로 출력하기
+ * https://www.acmicpc.net/problem/11718
+ */
+
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+input.forEach((data) => { console.log(data) });
